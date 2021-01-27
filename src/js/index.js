8c42e7e063b39aa88d0f456a3d66489c46a1aa0f
MillionIncredible <maxi.schwartz94@gmail.com>
2021-01-27 22:00:05 +0100

    fix for scrolling behind menu and start of 'Herkunft' site

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 document.getElementById("click-open").onclick = function openNav() {
   document.getElementById("mobile-menu").style.width = "100%";
+  document.body.style.overflow = "hidden";
 };
 
 document.getElementById("click-close").onclick = function closeNav() {
   document.getElementById("mobile-menu").style.width = "0";
+  document.body.style.overflow = "scroll";
 };
 
 /* tries that didn't work
