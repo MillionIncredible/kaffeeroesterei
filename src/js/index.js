8964faa33c713cb46d14a1e35c9e4e026b2188f7
MillionIncredible <maxi.schwartz94@gmail.com>
2021-01-27 21:09:26 +0100

    outsourcing js to external js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,34 @@
-function openNav() {
+document.getElementById("click-open").onclick = function openNav() {
   document.getElementById("mobile-menu").style.width = "100%";
+};
+
+document.getElementById("click-close").onclick = function closeNav() {
+  document.getElementById("mobile-menu").style.width = "0";
+};
+
+/* tries that didn't work
+const mobileMenu = document.getElementById("mobile-menu");
+const clickOpen = document.getElementById("click-open");
+const clickClose = document.getElementById("click-close");
+
+function openNav() {
+  mobileMenu.style.width = "100%";
+}
+
+function closeNav() {
+  mobileMenu.style.width = "0";
+}
+
+clickOpen.onclick = openNav();
+clickClose.onclick = closeNav();
+*/
+
+/*
+function openNav() {
+    document.getElementById("mobile-menu").style.width = "100%";
 }
 
 function closeNav() {
   document.getElementById("mobile-menu").style.width = "0";
 }
+*/
